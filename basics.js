a1f082dbbaf84f71bc4fad9ac9dd0e530b269f49
Harsh goel <126972459+Harshgoel001@users.noreply.github.com>
2024-03-12 12:50:47 +0530

    Add files via upload
    Basics learn by chai aur code

---
+++ basics.js
@@ -0,0 +1,9 @@
+const accountId = 734393;
+let accountName = "Harshgoel001";
+let accountPass = "ajfhjkah";
+accountMember = 'ahed'
+accountCity = 'Modinagar';
+/* we can not use var in current js as it creates issue in function scope and block scope*/ 
+
+let acId;
+console.table([accountId,accountName,accountPass,accountMember,accountCity,acId]);
