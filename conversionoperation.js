181dc070b6b8d2e5502aa06116fdc8191114916a
Harsh goel <126972459+Harshgoel001@users.noreply.github.com>
2024-03-12 12:51:41 +0530

    Add files via upload
    In this we have learn how to conversion of datatypes

---
+++ conversionoperation.js
@@ -0,0 +1,26 @@
+let name = "Harsh goel";
+let course = "B.tech";
+let age = "229ha";
+let gitprofile = "harshgoel001"
+
+//console.log(typeof null);
+
+let Name = 'hg';
+let ageH = 89;
+let noAge = Number(age)
+//console.log(noAge);
+// 33 =>Number
+//22ar =>NaN(Not a number)
+
+let isLoggedIn = "harsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1->true 0 =>false
+// ""=>false
+// "Harsh" =>true
+let number = 992
+
+let NewNumber = String(number);
+console.log(NewNumber);
+console.log(typeof(NewNumber));
